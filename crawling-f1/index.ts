418e05fc18e7923d98c6cc2f235459159b869803
x3pi <45082550+x3pi@users.noreply.github.com>
2023-06-27 12:20:42 +0700

    Update index.ts

--- crawling-f1/index.ts
+++ crawling-f1/index.ts
@@ -7,15 +7,15 @@ for (let i = 2023; i <= 2023; i++) {
         'https://www.formula1.com/en/results/jcr:content/resultsarchive.html/' + i + '/races.html',
         function (tablesAsJson: any) {
             for (let y = 0; y < tablesAsJson[0].length; y++) {
-                let GrandPrix = tablesAsJson[0][y]['Grand Prix'].toLowerCase().replace(' ', '-')
-               console.log(tablesAsJson);
-                // Tabletojson.convertUrl(
-                //     'https://www.formula1.com/en/results/jcr:content/resultsarchive.html/2023/races/' + GrandPrix + '/race-result.html',
-                //     function (tablesAsJson2: any) {
-                //         console.log(GrandPrix);
-                //         console.log(tablesAsJson2[0]);
-                //     }
-                // )
+                let GrandPrix = tablesAsJson[0][y]['Grand Prix'];
+                var words = GrandPrix.split('/');
+                words = words.splice(3).join('/')
+                Tabletojson.convertUrl(
+                    'https://www.formula1.com/en/results/jcr:content/resultsarchive.html/' + words,
+                    function (tablesAsJson2: any) {
+                        console.log(tablesAsJson2[0]);
+                    }
+                )
             }
         }
     )
