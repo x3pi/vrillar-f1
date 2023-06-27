94d83d553d82f22b6e3ca80d5ab5581ec12f2801
x3pi <45082550+x3pi@users.noreply.github.com>
2023-06-27 12:04:35 +0700

    Edit lib tabletojson

--- crawling-f1/index.ts
+++ crawling-f1/index.ts
@@ -2,9 +2,21 @@
 
 import { Tabletojson } from 'tabletojson';
 
-Tabletojson.convertUrl(
-    'https://www.formula1.com/en/results.html/1990/races.html',
-    function (tablesAsJson: any) {
-        console.log(tablesAsJson[0]);
-    }
-)
+for (let i = 2023; i <= 2023; i++) {
+    Tabletojson.convertUrl(
+        'https://www.formula1.com/en/results/jcr:content/resultsarchive.html/' + i + '/races.html',
+        function (tablesAsJson: any) {
+            for (let y = 0; y < tablesAsJson[0].length; y++) {
+                let GrandPrix = tablesAsJson[0][y]['Grand Prix'].toLowerCase().replace(' ', '-')
+               console.log(tablesAsJson);
+                // Tabletojson.convertUrl(
+                //     'https://www.formula1.com/en/results/jcr:content/resultsarchive.html/2023/races/' + GrandPrix + '/race-result.html',
+                //     function (tablesAsJson2: any) {
+                //         console.log(GrandPrix);
+                //         console.log(tablesAsJson2[0]);
+                //     }
+                // )
+            }
+        }
+    )
+}
